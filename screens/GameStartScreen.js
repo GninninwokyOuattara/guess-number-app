cfc8f6b271e5c8c22f937b51ce2d150f2486229b
Gninninwoky Ouattara <idrissgouattara@gmail.com>
2021-02-08 23:22:01 +0000

    Keyboard dismissal when touch outside input field

--- screens/GameStartScreen.js
+++ screens/GameStartScreen.js
@@ -1,5 +1,13 @@
 import React, { useState } from "react";
-import { View, Text, StyleSheet, TextInput, Button } from "react-native";
+import {
+    View,
+    Text,
+    StyleSheet,
+    TextInput,
+    Button,
+    TouchableWithoutFeedback,
+    Keyboard,
+} from "react-native";
 import Card from "../components/Card";
 import Input from "../components/Input";
 import Color from "../constants/colors";
@@ -11,30 +19,32 @@ const GameStartScreen = () => {
         setEnteredInput(inputText.replace(/[^0-9]/g, ""));
     };
     return (
-        <View style={styles.screen}>
-            <Text style={styles.startGame}>Start a New Game!</Text>
-            <Card style={styles.inputContainer}>
-                <Text>Select a number</Text>
-                <Input
-                    style={styles.numberInput}
-                    blurOnSubmit
-                    autoCapitalize="none"
-                    autoCorrect={false}
-                    keyboardType="number-pad"
-                    maxLength={2}
-                    onChangeText={inputHandler}
-                    value={enteredInput}
-                />
-                <View style={styles.buttonContainer}>
-                    <View style={styles.button}>
-                        <Button title="Reset" color={Color.accent} />
+        <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
+            <View style={styles.screen}>
+                <Text style={styles.startGame}>Start a New Game!</Text>
+                <Card style={styles.inputContainer}>
+                    <Text>Select a number</Text>
+                    <Input
+                        style={styles.numberInput}
+                        blurOnSubmit
+                        autoCapitalize="none"
+                        autoCorrect={false}
+                        keyboardType="number-pad"
+                        maxLength={2}
+                        onChangeText={inputHandler}
+                        value={enteredInput}
+                    />
+                    <View style={styles.buttonContainer}>
+                        <View style={styles.button}>
+                            <Button title="Reset" color={Color.accent} />
+                        </View>
+                        <View style={styles.button}>
+                            <Button title="Confirm" color={Color.primary} />
+                        </View>
                     </View>
-                    <View style={styles.button}>
-                        <Button title="Confirm" color={Color.primary} />
-                    </View>
-                </View>
-            </Card>
-        </View>
+                </Card>
+            </View>
+        </TouchableWithoutFeedback>
     );
 };
 
