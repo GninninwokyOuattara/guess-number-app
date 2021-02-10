337d5802aadb46c43a77174fbbd34f49bb48c8fb
Gninninwoky Ouattara <idrissgouattara@gmail.com>
2021-02-10 13:48:34 +0000

    styling modification and styling of game over message

--- screens/GameOverScreen.js
+++ screens/GameOverScreen.js
@@ -1,10 +1,11 @@
 import React from "react";
 import { View, Text, StyleSheet, Button, Image } from "react-native";
+import Color from "../constants/colors";
 
 const GameOverScreen = (props) => {
     return (
         <View style={styles.screen}>
-            <Text>Game Over</Text>
+            <Text style={styles.textContainer}>Game Over</Text>
             <View style={styles.imageContainer}>
                 {/* <Image
                     source={require("../assets/success.png")}
@@ -20,8 +21,16 @@ const GameOverScreen = (props) => {
                     resizeMode="cover"
                 />
             </View>
-            <Text>In {props.roundNumber}</Text>
-            <Text>The number was {props.number}</Text>
+            {/* <Text>In {props.roundNumber}</Text>
+            <Text>The number was {props.number}</Text> */}
+            <View style={styles.resultContainer}>
+                <Text style={styles.textContainer}>
+                    Your phone guessed the right number{" "}
+                    <Text style={styles.highlight}>{props.number}</Text> after{" "}
+                    <Text style={styles.highlight}>{props.roundNumber} </Text>{" "}
+                    round
+                </Text>
+            </View>
             <Button title="NEW GAME" onPress={props.onNewGame} />
         </View>
     );
@@ -50,6 +59,20 @@ const styles = {
         // borderColor: "black",
         height: "100%",
         width: "100%",
+    },
+
+    resultContainer: {
+        // marginHorizontal: 40,
+        marginVertical: 20,
+    },
+
+    textContainer: {
+        textAlign: "center",
+        fontFamily: "open-sans-bold",
+    },
+
+    highlight: {
+        color: Color.accent,
     },
 };
 
